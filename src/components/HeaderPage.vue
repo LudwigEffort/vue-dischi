43dd2c515ae9a4d30ab90fdf5c3dcc1c6057473c
LudwigEffort <dr.feynman65@gmail.com>
2022-11-28 23:49:50 +0100

    add components

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nav class="navbar bg-light">
+      <div class="container">
+        <a
+          class="navbar-brand"
+          href="#"
+        >
+          <img
+            src="../assets/spotify-logo.png"
+            alt="Bootstrap"
+            width="30"
+            height="30"
+          >
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderPage',
+};
+</script>
+
+<style lang="scss" scoped>
+    div {
+        background-color: #2E3A46;
+    }
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  data() {
+    return {
+      arrDisco: null,
+    };
+  },
+  created() {
+    // download data
+  },
+};
+</script>
+
+<style>
+
+</style>
