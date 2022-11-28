8e0f28c9fbf2323134c7827269cfd26b8677c240
LudwigEffort <dr.feynman65@gmail.com>
2022-11-29 00:13:31 +0100

    add axios script and v-for on ul

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,17 +1,36 @@
 <template>
-  <div />
+  <div>
+    <ul v-if="arrDisco">
+      <li
+        v-for="disco in arrDisco"
+        :key="disco.title"
+      >
+        {{ disco.title }}
+      </li>
+    </ul>
+    <div v-else>
+      Loading...
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'MainPage',
   data() {
     return {
       arrDisco: null,
+      urlAPI: 'https://flynn.boolean.careers/exercises/api/array/music',
     };
   },
   created() {
-    // download data
+    axios.get(this.urlAPI)
+      .then((axiosResponse) => {
+        console.log(axiosResponse);
+        this.arrDisco = axiosResponse.data.response;
+      });
   },
 };
 </script>
